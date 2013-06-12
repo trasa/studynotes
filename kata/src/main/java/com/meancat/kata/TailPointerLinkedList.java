465cf50ff25a04808f2747a6129db5aae04641a2
Tony Rasa <trasa@meancat.com>
2013-06-11 17:53:26 -0700

    Single lists, with and without tail pointers

--- kata/src/main/java/com/meancat/kata/TailPointerLinkedList.java
+++ kata/src/main/java/com/meancat/kata/TailPointerLinkedList.java
@@ -6,46 +6,52 @@ import java.util.List;
 
 import static com.google.common.collect.Lists.newArrayList;
 
-public class SingleLinkedList<T> {
+public class TailPointerLinkedList<T> {
 
     protected Node<T> head;
+    protected Node<T> tail;
 
     public void addToHead(T value) {
         Node<T> newNode = new Node<T>(value);
-        newNode.next = head;
-        head = newNode;
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
     }
 
     public void addToTail(T value) {
         Node<T> newNode = new Node<T>(value);
-
-        if (head == null) {
+        if (tail == null) {
             head = newNode;
+            tail = newNode;
         } else {
-            Node<T> cur = head;
-            while(cur.next != null) {
-                cur = cur.next;
-            }
-            cur.next = newNode;
+            tail.next = newNode;
+            tail = newNode;
         }
     }
 
     public void remove(T value) {
-        Node<T> prev;
-        Node<T> cur;
-
         if (head == null) {
             return;
         }
         if (head.value.equals(value)) {
+            if (tail == head) {
+                tail = head.next;
+            }
             head = head.next;
             return;
         }
-        prev = head;
-        cur = head.next;
-        while (cur != null) {
+        Node<T> cur = head.next;
+        Node<T> prev = head;
+        while(cur != null) {
             if (cur.value.equals(value)) {
                 prev.next = cur.next;
+                if (cur == tail) {
+                    tail = prev;
+                }
                 return;
             }
             prev = cur;
