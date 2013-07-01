6a3f714c4c87b4f0d4b2355053f9abe9224a2e51
Tony Rasa <trasa+github@meancat.com>
2013-06-30 23:42:45 -0700

    MessageQueue Problem
    The std::queue being used is not thread-safe, all access to the queue must
    be limited by use of a mutex.

--- meteor/MessageQueue/MessageQueue.cpp
+++ meteor/MessageQueue/MessageQueue.cpp
@@ -26,7 +26,11 @@ private:
 
 	vector<HANDLE> m_workerThreads;
 
+	HANDLE m_queueMutex;
+
 	void DispatchOneMessage();
+
+	bool GetOneMessage(int* message);
 
 	static DWORD WINAPI StaticWorkerThread(LPVOID param);
 	void WorkerThread();
@@ -43,6 +47,8 @@ MessageQueue::MessageQueue(int numThreads, IMessageWriter* writer)
 {
 	m_messageWriter = writer;
 
+	m_queueMutex = CreateMutex(NULL, FALSE, NULL);
+
 	m_shuttingDown = false;
 
 	for(int i=0;i<numThreads;i++)
@@ -56,22 +62,74 @@ MessageQueue::~MessageQueue()
 {
 	m_shuttingDown = true;
 	WaitForMultipleObjects(m_workerThreads.size(), &m_workerThreads[0], TRUE, INFINITE);
+	if (m_queueMutex) 
+	{
+		CloseHandle(m_queueMutex);
+		m_queueMutex = NULL;
+	}
 }
 
 void MessageQueue::EnqueueOutgoingMessage(int message)
 {
-	m_messageQueue.push(message);
+	// need the queueMutex before we can modify the queue
+	DWORD waitResult = WaitForSingleObject(m_queueMutex, -1);
+	if (waitResult == WAIT_OBJECT_0) 
+	{
+		__try
+		{
+			m_messageQueue.push(message);
+		}
+		__finally 
+		{
+			ReleaseMutex(m_queueMutex);
+		}
+	} 
+	else 
+	{
+		// error while trying to aquire mutex.
+		throw(waitResult);
+	}
 }
 
 
 void MessageQueue::DispatchOneMessage()
 {
-	if(!m_messageQueue.empty())
+	int message;
+	if (GetOneMessage(&message))
 	{
-		int message = m_messageQueue.front();
-		m_messageQueue.pop();
+		m_messageWriter->WriteMessage(message);	
+	}
+}
 
-		m_messageWriter->WriteMessage(message);
+// Safely get the next message off of the queue, if there is one.
+// returns false if we have no messages.
+bool MessageQueue::GetOneMessage(int* message)
+{
+	DWORD waitResult = WaitForSingleObject(m_queueMutex, -1);
+	if (waitResult == WAIT_OBJECT_0) 
+	{
+		__try 
+		{
+			if(!m_messageQueue.empty())
+			{
+				*message = m_messageQueue.front();
+				m_messageQueue.pop();
+				return true;
+			}
+			else 
+			{
+				return false;
+			}
+		}
+		__finally
+		{
+			ReleaseMutex(m_queueMutex);
+		}
+	} 
+	else 
+	{
+		// failed to aquire the mutex, things are bad.
+		throw(waitResult);
 	}
 }
 
